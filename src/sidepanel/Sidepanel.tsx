1148836a3e01e53c930cb0fadec8cad6acda1c33
Tommy <35355900+thomasgrz@users.noreply.github.com>
2025-11-17 18:02:12 -0800

    refactor: rename some files (#2)
    Co-authored-by: thomasgrz <thomasgrz@outlok.com>

--- src/sidepanel/Sidepanel.tsx
+++ src/sidepanel/Sidepanel.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import "./Sidepanel.css";
+
+import "@radix-ui/themes/styles.css";
+
+import { Dashboard } from "@/components/Dashboard/Dashboard";
+import { Theme } from "@radix-ui/themes";
 
 const node = document.getElementById("root");
 if (node instanceof HTMLElement) {
   createRoot(node).render(
     <StrictMode>
-      <App />
+      <Theme radius="full">
+        <Dashboard />
+      </Theme>
     </StrictMode>,
   );
 }
